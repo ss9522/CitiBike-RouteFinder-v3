1d014583b974c7505b50119cf1ea25130cfd21cb
Shreyas Sriram <s.sriram.w@gmail.com>
2024-08-05 17:18:34 -0400

    Added heroku postbuild script for React

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const mongoose = require('./db'); // Load and run the db.js file
 const User = require('./models/User');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON bodies
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+const buildPath = path.join(__dirname, 'client/build');
+console.log(`Serving static files from ${buildPath}`);
+app.use(express.static(buildPath));
 
 // API route (example)
 app.get('/api/hello', (req, res) => {
@@ -41,7 +43,14 @@ app.get('/api/users', async (req, res) => {
 
 // Catchall handler to serve the React app for any other request
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  const indexPath = path.join(buildPath, 'index.html');
+  console.log(`Serving index.html from ${indexPath}`);
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      console.error(`Error serving index.html: ${err}`);
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.listen(PORT, () => {
