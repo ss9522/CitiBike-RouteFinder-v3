9d29ce5e20ed1768371ce9808e14282824a1aa43
Shreyas Sriram <s.sriram.w@gmail.com>
2024-08-08 15:18:47 -0400

    Requiring SSL for DB conns.

--- server.js
+++ server.js
@@ -8,12 +8,29 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const { Pool } = require('pg');
 
+const { Pool } = require('pg');
+
 const pool = new Pool({
   connectionString: process.env.FEEDBACK_DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  },
-  connectionTimeoutMillis: 10000
+    rejectUnauthorized: false,
+    sslmode: 'require'
+  }
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+// Test the connection
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error('Error connecting to the database', err);
+  } else {
+    console.log('Successfully connected to the database');
+    done();
+  }
 });
 // Middleware for parsing JSON bodies and new helmet
 app.use(express.json());
