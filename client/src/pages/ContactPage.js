684e4fc9921dbdf41a9687485a41fe4600a43dfb
Shreyas Sriram <s.sriram.w@gmail.com>
2024-08-08 13:28:18 -0400

    Attempted fix on form in contact page

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import FeedbackForm from '../components/FeedbackForm';
 
 function ContactPage() {
   return (
     <div className="page-content">
       <h1>Contact Us</h1>
       <p>If you have any questions or feedback, feel free to reach out!</p>
+      <div>
+        <h2>Feedback Form</h2>
+        <p>Send us your feedback below.</p>
+        <FeedbackForm />
+      </div>
     </div>
   );
 }
