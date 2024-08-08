cf5b824a7887863748b8765446491fe2162f02cd
Shreyas Sriram <s.sriram.w@gmail.com>
2024-08-07 21:38:55 -0400

    Moved key to HTML and simplified MapPage.js

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { APIProvider } from '@vis.gl/react-google-maps';
 import GoogleMaps from '../components/GoogleMaps';
 
 const MapPage = () => {
-  const handleMapLoad = () => {
-    console.log('Maps API has loaded.');
-  };
-
   return (
     <div className="page-content">
       <div className="map-page">
         <h1>Map</h1>
-        <APIProvider 
-          apiKey={"AIzaSyA16IE_D0J-w-i5LqWcRjHWTWPWmmQxQyA"} 
-          onLoad={handleMapLoad}
-        >
-          <GoogleMaps />
-        </APIProvider>
+        <GoogleMaps />
       </div>
     </div>
   );
