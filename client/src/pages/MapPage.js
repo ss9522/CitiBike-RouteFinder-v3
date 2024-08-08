ce356a490a9cb6068dbd310ff200a0fc386aa004
Shreyas Sriram <s.sriram.w@gmail.com>
2024-08-07 21:50:25 -0400

    Reintro'd APIProvider to MapPage.js

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { APIProvider } from '@vis.gl/react-google-maps';
 import GoogleMaps from '../components/GoogleMaps';
 
 const MapPage = () => {
+  const handleMapLoad = () => {
+    console.log('Maps API has loaded.');
+  };
+
   return (
     <div className="page-content">
       <div className="map-page">
         <h1>Map</h1>
-        <GoogleMaps />
+        <APIProvider 
+          apiKey={"AIzaSyCPpQ44Ra5k-x_mjUTv2o3XtSjbIIkfbUA"} 
+          onLoad={handleMapLoad}
+        >
+          <GoogleMaps />
+        </APIProvider>
       </div>
     </div>
   );
